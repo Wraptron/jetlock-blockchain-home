d93a740bcc814cdf29a24baffef58832e12db3ae
uttam-sharma786 <uttamsharma.8560@gmail.com>
2025-04-24 17:12:43 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      
+       <h1 className= "bg-amber-300">Hello Exchange world</h1>
+      <p className="read-the-docs">
+       Exchange Currency
+      </p>
+    </>
+  )
+}
+
+export default App
